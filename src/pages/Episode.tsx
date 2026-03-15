0e8ad3c43c91528d713781d3ea2ae7b8fe45a983
Tatiana <kartasheva.tatiana@gmail.com>
2026-03-15 20:29:47 +0300

    feat: add simple episodes

--- src/pages/Episode.tsx
+++ src/pages/Episode.tsx
@@ -1,5 +1,19 @@
+import { useMemo } from "react";
+import { useParams, useNavigate } from "react-router";
+import { episodes } from '../data/episodes';
+
 export function Episode() {
+    const params = useParams();
+    const navigate = useNavigate();
+
+    const episode = useMemo(() => {
+        return episodes.filter(e => String(e.id) === params.id)[0] || null;
+    }, [episodes]);
+
+    if (!episode)
+        navigate('/');
+
     return (
-        <h1>Эпизод</h1>
+        <h1>{episode.name}</h1>
     );
 };
--- src/pages/Episodes.tsx
+++ src/pages/Episodes.tsx
@@ -1,5 +1,14 @@
+import { Link } from "react-router";
+import { episodes } from '../data/episodes';
+
 export function Episodes() {
     return (
-        <h1>Эпизоды</h1>
+        <>
+            <h1>Эпизоды</h1>
+
+            {episodes.map(episode => (
+                <Link to={`/episodes/${episode.id}`}>{episode.name}</Link>
+            ))}
+        </>
     );
 };
