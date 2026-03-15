49aa0ff9cbb12d6c7638cd860255c4e082235e10
Tatiana <kartasheva.tatiana@gmail.com>
2026-03-15 20:27:07 +0300

    feat: add simple Locations

--- src/pages/Location.tsx
+++ src/pages/Location.tsx
@@ -1,5 +1,19 @@
+import { useMemo } from "react";
+import { useParams, useNavigate } from "react-router";
+import { locations } from '../data/locations';
+
 export function Location() {
+    const params = useParams();
+    const navigate = useNavigate();
+
+    const location = useMemo(() => {
+        return locations.filter(l => String(l.id) === params.id)[0] || null;
+    }, [locations]);
+
+    if (!location)
+        navigate('/');
+
     return (
-        <h1>Локация</h1>
+        <h1>{location.name}</h1>
     );
 };
--- src/pages/Locations.tsx
+++ src/pages/Locations.tsx
@@ -1,5 +1,14 @@
+import { Link } from "react-router";
+import { locations } from '../data/locations';
+
 export function Locations() {
     return (
-        <h1>Локации</h1>
+        <>
+            <h1>Локации</h1>
+
+            {locations.map(location => (
+                <Link to={`/locations/${location.id}`}>{location.name}</Link>
+            ))}
+        </>
     );
 };
