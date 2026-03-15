74c11107404dd9121bdf4c4f108cb4ab2fe85deb
Tatiana <kartasheva.tatiana@gmail.com>
2026-03-15 19:47:31 +0300

    feat: add props to Characters

--- src/pages/Characters.tsx
+++ src/pages/Characters.tsx
@@ -1,4 +1,6 @@
-export function Characters() {
+import type { CharactersProps } from "../types/characters";
+
+export function Characters({ characters }: CharactersProps) {
     return (
         <h1>Персонажи</h1>
     );
