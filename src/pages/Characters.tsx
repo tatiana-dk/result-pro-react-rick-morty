16de9f7d450ca3b8bc60cd4ce4ac4650c0d384db
Tatiana <kartasheva.tatiana@gmail.com>
2026-03-15 19:54:08 +0300

    feat: add simple data to Characters page

--- src/pages/Characters.tsx
+++ src/pages/Characters.tsx
@@ -1,7 +1,17 @@
 import type { CharactersProps } from "../types/characters";
+import { Link } from "react-router";
 
 export function Characters({ characters }: CharactersProps) {
     return (
-        <h1>Персонажи</h1>
+        <>
+            <h1>Персонажи</h1>
+
+            {characters.map(character => (
+                <Link to={`/characters/:${character.id}`}>
+                    <img src={character.image} /><br />
+                    <span>{character.name}</span>
+                </Link>
+            ))}
+        </>
     );
 };
