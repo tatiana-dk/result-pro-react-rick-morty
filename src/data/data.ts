febb6e7a38e1f03f1cf832f5154648449dd137f2
Tatiana <kartasheva.tatiana@gmail.com>
2026-03-16 09:21:02 +0300

    feat: add single object for data

---
+++ src/data/data.ts
@@ -0,0 +1,9 @@
+import { characters } from './characters';
+import { locations } from './locations';
+import { episodes } from './episodes';
+
+export const data = {
+    characters,
+    locations,
+    episodes
+};
