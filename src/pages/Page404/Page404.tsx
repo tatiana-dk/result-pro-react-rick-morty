5f44b0f171a18dea1a3a044ae7094b06b51129b1
Tatiana <kartasheva.tatiana@gmail.com>
2026-03-29 08:45:07 +0300

    refactor: add reexport to pages

---
+++ src/pages/Page404/Page404.tsx
@@ -0,0 +1,9 @@
+import { lang } from "../../lang";
+
+export function Page404() {
+    return (
+        <>
+            <h1>{lang[404].heading}</h1>
+        </>
+    );
+}
--- src/pages/Page404/index.tsx
+++ src/pages/Page404/index.tsx
@@ -1,9 +1,1 @@
-import { lang } from "../../lang";
-
-export function Page404() {
-    return (
-        <>
-            <h1>{lang[404].heading}</h1>
-        </>
-    );
-}
+export * from './Page404';
