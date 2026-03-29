bec621720c31c0b2dfb6e4c3c66c310ed8643b7c
Tatiana <kartasheva.tatiana@gmail.com>
2026-03-29 19:00:44 +0300

    feat: remove DataProvider

--- src/pages/Category/ui/CategoryLayout.tsx
+++ src/pages/Category/ui/CategoryLayout.tsx
@@ -1,7 +1,6 @@
 import { Outlet, useParams } from "react-router";
 import { CategoryMenu } from "@/pages/Category/ui/CategoryMenu";
 import { useMemo } from "react";
-import { DataProvider } from '@shared/providers/Data';
 
 import type { CategoryName } from "@/shared/config/types";
 import ErrorBoundary from "@app/error/ErrorBoundary";
@@ -17,13 +16,11 @@ export function CategoryLayout() {
     }, [category]);        
 
     return (
-        <DataProvider>
-            <div className="content-grid">
-                <ErrorBoundary>{isCategoryValid && <CategoryMenu category={category as CategoryName} />}</ErrorBoundary>
-                <div className="content">
-                    <ErrorBoundary><Outlet /></ErrorBoundary>
-                </div>
+        <div className="content-grid">
+            <ErrorBoundary>{isCategoryValid && <CategoryMenu category={category as CategoryName} />}</ErrorBoundary>
+            <div className="content">
+                <ErrorBoundary><Outlet /></ErrorBoundary>
             </div>
-        </DataProvider>
+        </div>
     );
 };
