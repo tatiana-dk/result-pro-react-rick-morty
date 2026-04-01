f69d8c614f5d167083a6175d3e8887f916537d5f
Tatiana <kartasheva.tatiana@gmail.com>
2026-04-01 12:14:08 +0300

    feat: add Container

--- src/app/ui/MainLayout.tsx
+++ src/app/ui/MainLayout.tsx
@@ -2,15 +2,18 @@ import { Outlet } from "react-router";
 import { Header } from "@widgets/Header";
 import { Suspense } from "react";
 import ErrorBoundary from "@app/error/ErrorBoundary";
+import { Container } from '@mantine/core';
 
 export function MainLayout() {
     return (
         <div className="page">
             <Header />
             <ErrorBoundary>
-                <Suspense fallback={'Loading...'}>
-                    <Outlet />
-                </Suspense>
+                <Container size="xs">
+                    <Suspense fallback={'Loading...'}>
+                        <Outlet />
+                    </Suspense>
+                </Container>
             </ErrorBoundary>
         </div>
     );
