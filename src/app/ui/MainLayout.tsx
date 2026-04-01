ccfb59db1142e6be9c484354f1c44600742dccbf
Tatiana <kartasheva.tatiana@gmail.com>
2026-04-01 12:48:17 +0300

    feat: add AppShell to MainLayout

--- src/app/ui/MainLayout.tsx
+++ src/app/ui/MainLayout.tsx
@@ -2,19 +2,43 @@ import { Outlet } from "react-router";
 import { Header } from "@widgets/Header";
 import { Suspense } from "react";
 import ErrorBoundary from "@app/error/ErrorBoundary";
-import { Container } from "@/shared/ui";
+
+import { AppShell, Burger } from '@mantine/core';
+import { useDisclosure } from '@mantine/hooks';
 
 export function MainLayout() {
+    const [opened, { toggle }] = useDisclosure();
+
     return (
-        <div className="page">
-            <Header />
-            <ErrorBoundary>
-                <Container>
+        <AppShell
+            padding="md"
+            header={{ height: 60 }}
+            navbar={{
+                width: 300,
+                breakpoint: 'sm',
+                collapsed: { mobile: !opened },
+            }}
+        >
+            <AppShell.Header>
+                <Burger
+                    opened={opened}
+                    onClick={toggle}
+                    hiddenFrom="sm"
+                    size="sm"
+                />
+
+                <Header />
+            </AppShell.Header>
+
+            <AppShell.Navbar>Navbar</AppShell.Navbar>
+
+            <AppShell.Main>
+                <ErrorBoundary>
                     <Suspense fallback={'Loading...'}>
                         <Outlet />
                     </Suspense>
-                </Container>
-            </ErrorBoundary>
-        </div>
+                </ErrorBoundary>
+            </AppShell.Main>
+        </AppShell>
     );
 };
