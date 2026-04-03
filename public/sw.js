ba2517622bdaac8fa317dbadd41e1eabba5c147d
Tatiana <kartasheva.tatiana@gmail.com>
2026-04-03 20:20:48 +0300

    feat: add static cache

--- public/sw.js
+++ public/sw.js
@@ -2,13 +2,28 @@ const staticCacheName = 'static-site';
 
 const ASSETS = [
     '/',
-    
+    '/assets/android-chrome-192x192-Dvp2Ij_W.png',
+    '/assets/axios-BeeNzXPA.js',
+    '/assets/Category-CB2aHWDl.js',
+    '/assets/chunk-LFPYN7LY-qS3M-zyw.js',
+    '/assets/Detail-BFzgRGKe.js',
+    '/assets/Home-CotzyXsG.js',
+    '/assets/index--izu_zk4.js',
+    '/assets/index-B3BhHTpu.css',
+    '/assets/jsx-runtime-D-oznMWL.js',
+    '/assets/Login-BUbWHoAd.js',
+    '/assets/Page404-lGsbjXQN.js',
+    '/app.js',
+    '/favicon.svg',
+    '/icons.svg',
+    '/index.html'
 ];
 
 self.addEventListener('install', (event) => {
     caches.open(staticCacheName)
         .then(cache => {
-            cache.addAll()
+            console.log('Caches add ASSETS');
+            cache.addAll(ASSETS)
         });
 });
 
