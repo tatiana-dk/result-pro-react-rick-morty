5b550dcebb652fc51f4c37bf0dd184a109b09e6f
Tatiana <kartasheva.tatiana@gmail.com>
2026-04-04 11:50:03 +0300

    refactor: add async await

--- public/sw.js
+++ public/sw.js
@@ -1,36 +1,57 @@
-const staticCacheName = 'static-site';
+const staticCacheName = 'static-site-v3';
+const dynamicCacheName = 'dynamic-site-v2';
+const page404Name = '';
 
 const ASSETS = [
     '/',
-    '/assets/android-chrome-192x192-Dvp2Ij_W.png',
-    '/assets/axios-BeeNzXPA.js',
-    '/assets/Category-CB2aHWDl.js',
-    '/assets/chunk-LFPYN7LY-qS3M-zyw.js',
-    '/assets/Detail-BFzgRGKe.js',
-    '/assets/Home-CotzyXsG.js',
-    '/assets/index--izu_zk4.js',
-    '/assets/index-B3BhHTpu.css',
-    '/assets/jsx-runtime-D-oznMWL.js',
-    '/assets/Login-BUbWHoAd.js',
-    '/assets/Page404-lGsbjXQN.js',
     '/app.js',
     '/favicon.svg',
     '/icons.svg',
-    '/index.html'
+    '/index.html',
+    '/manifest.json',
+    '/404'
 ];
 
-self.addEventListener('install', (event) => {
-    caches.open(staticCacheName)
-        .then(cache => {
-            console.log('Caches add ASSETS');
-            cache.addAll(ASSETS)
-        });
+self.addEventListener('install', async (event) => {
+    const cache = await caches.open(staticCacheName);
+    await cache.addAll(ASSETS);
 });
 
-self.addEventListener('activate', (event) => {
-    console.log('activated')
+self.addEventListener('activate', async (event) => {
+    const cachesKeysArr = await caches.keys();
+    await Promise.all(
+        cachesKeysArr
+            .filter(key => key !== staticCacheName && key !== dynamicCacheName)
+            .map(key => caches.delete(key))
+    );
 })
 
 self.addEventListener('fetch', (event) => {
-    console.log('fetch', event.request.url)
-})
+    event.respondWith(cacheFirst(event.request));
+});
+
+async function cacheFirst(request) {
+    const cached = await caches.match(request);
+    try {
+        return cached ?? await fetch(request).then(response => {
+            return networkFirst(request);
+        });
+    } catch(error) {
+        console.log(1, error);
+        return networkFirst(request);
+    }
+    
+}
+
+async function networkFirst(request) {
+    const cache = await caches.open(dynamicCacheName);
+    try {
+        const response = await fetch(request);
+        await cache.put(request, response.clone());
+        return response;
+    } catch(error) {
+        console.log(2, error);
+        const cached = await caches.match(request);
+        return cached ?? await caches.match('/404');
+    }
+}
